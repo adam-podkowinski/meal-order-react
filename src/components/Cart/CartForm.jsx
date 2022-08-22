148a82b16875dbdefb4cdfacf10d9b9d074491e7
adampodkdev@gmail.com <adampodkdev@gmail.com>
2022-08-22 17:42:39 +0200

    Finish cart form UI

--- src/components/Cart/CartForm.jsx
+++ src/components/Cart/CartForm.jsx
@@ -8,7 +8,6 @@ const emailPattern = {
   message: emailPatternMessage,
 };
 
-//TODO: On bigger screens show order button and cart items on the side
 const CartForm = ({ isEmpty, visible }) => {
   const {
     register,
@@ -20,40 +19,59 @@ const CartForm = ({ isEmpty, visible }) => {
     console.log(`Order: ${d}`);
   };
 
-  const { firstName, lastName, email, country, city, address, payment } =
-    errors;
+  const { firstName, lastName, email, country, address, payment } = errors;
+
+  const errorMessage = (input) => {
+    if (input?.type === null) return null;
+    if (input?.type === "required") return "Field is required";
+    if (input?.type === "pattern") return "Invalid e-mail";
+  };
 
   return (
     <form
       style={visible ? {} : { display: "none" }}
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          firstName?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"first-name"}>First Name: </label>
-          <p>{firstName?.type}</p>
+          <p>{errorMessage(firstName)}</p>
         </div>
         <input
           {...register("firstName", { required: true })}
           type="text"
           id={"first-name"}
+          placeholder={"John"}
         />
       </div>
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          lastName?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"last-name"}>Last Name: </label>
-          <p>{lastName?.type}</p>
+          <p>{errorMessage(lastName)}</p>
         </div>
         <input
           {...register("lastName", { required: true })}
           type="text"
           id={"last-name"}
+          placeholder={"Smith"}
         />
       </div>
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          email?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"email"}>E-mail: </label>
-          <p>{email?.type}</p>
+          <p>{errorMessage(email)}</p>
         </div>
         <input
           {...register("email", {
@@ -62,45 +80,49 @@ const CartForm = ({ isEmpty, visible }) => {
           })}
           type="email"
           id={"email"}
+          placeholder={"email@mail.com"}
         />
       </div>
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          country?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"country"}>Country: </label>
-          <p>{country?.type}</p>
+          <p>{errorMessage(country)}</p>
         </div>
         <input
           {...register("country", { required: true })}
           type="text"
           id={"country"}
+          placeholder={"United States"}
         />
       </div>
-      <div className={classes.inputBox}>
-        <div className={classes.infoBox}>
-          <label htmlFor={"city"}>City: </label>
-          <p>{city?.type}</p>
-        </div>
-        <input
-          {...register("city", { required: true })}
-          type="text"
-          id={"city"}
-        />
-      </div>
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          address?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"address"}>Address: </label>
-          <p>{address?.type}</p>
+          <p>{errorMessage(address)}</p>
         </div>
         <input
           {...register("address", { required: true })}
           type="text"
           id={"address"}
+          placeholder={"9970 Pilgrim Street Brooklyn, NY 11214"}
         />
       </div>
-      <div className={classes.inputBox}>
+      <div
+        className={`${classes.inputBox} ${
+          payment?.type ? classes.invalidInput : undefined
+        }`}
+      >
         <div className={classes.infoBox}>
           <label htmlFor={"payment"}>Payment: </label>
-          <p>{payment?.type}</p>
+          <p>{errorMessage(payment)}</p>
         </div>
         <select {...register("payment", { required: true })} id="payment">
           <option value="">Select...</option>
